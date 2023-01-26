879ff4de9ac19d9ebe7857571a57b314dd05b356
Helena Makendengue <helenamakens1013@gmail.com>
2023-01-26 13:32:35 -0600

    created cart component

--- 02 - Cart Component/11 - Creating Our Cart Component/lemonadestand/src/app/lemonade/glass/glass.component.ts
+++ 02 - Cart Component/11 - Creating Our Cart Component/lemonadestand/src/app/lemonade/glass/glass.component.ts
@@ -3,17 +3,16 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-glass',
   templateUrl: './glass.component.html',
-  styleUrls: ['./glass.component.css']
+  styleUrls: ['./glass.component.css'],
 })
 export class GlassComponent implements OnInit {
+  @Input() percentLemonJuice: number = 0;
+  @Input() percentSugar: number = 0;
+  @Input() numberOfIceCubes: number = 0;
 
-  @Input() percentLemonJuice: number = 0
-  @Input() percentSugar: number = 0
-  @Input() numberOfIceCubes: number = 0
+  @Input() classNames: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
