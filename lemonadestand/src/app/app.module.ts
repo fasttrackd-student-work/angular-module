04651b0324dbdd03f5d767a6e72b5462fbc56f9f
Helena Makendengue <helenamakens1013@gmail.com>
2023-02-27 17:15:05 -0600

    basic routing

--- lemonadestand/src/app/app.module.ts
+++ lemonadestand/src/app/app.module.ts
@@ -1,20 +1,26 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { ReactiveFormsModule } from '@angular/forms'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component'
-import { LemonadeComponent } from './lemonade/lemonade.component'
-import { ProductComponent } from './lemonade/product/product.component'
-import { CardComponent } from './lemonade/card/card.component'
-import { ConvertMeasurementPipe } from './lemonade/product/convert-measument.pipe'
-import { GlassComponent } from './lemonade/glass/glass.component'
-import { LiquidComponent } from './lemonade/glass/liquid/liquid.component'
-import { WaveComponent } from './lemonade/glass/liquid/wave/wave.component'
-import { CartComponent } from './cart/cart.component'
-import { CartItemComponent } from './cart/cart-item/cart-item.component'
-import { CartIconComponent } from './lemonade/cart-icon/cart-icon.component'
-import { CustomerFormComponent } from './customer-form/customer-form.component'
-import { InputComponent } from './customer-form/input/input.component'
+import { AppComponent } from './app.component';
+import { LemonadeComponent } from './lemonade/lemonade.component';
+import { ProductComponent } from './lemonade/product/product.component';
+import { CardComponent } from './lemonade/card/card.component';
+import { ConvertMeasurementPipe } from './lemonade/product/convert-measument.pipe';
+import { GlassComponent } from './lemonade/glass/glass.component';
+import { LiquidComponent } from './lemonade/glass/liquid/liquid.component';
+import { WaveComponent } from './lemonade/glass/liquid/wave/wave.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItemComponent } from './cart/cart-item/cart-item.component';
+import { CartIconComponent } from './lemonade/cart-icon/cart-icon.component';
+import { CustomerFormComponent } from './customer-form/customer-form.component';
+import { InputComponent } from './customer-form/input/input.component';
+
+const routes: Routes = [
+  { path: 'lemonade', component: LemonadeComponent },
+  { path: '', component: CustomerFormComponent },
+];
 
 @NgModule({
   declarations: [
@@ -32,11 +38,8 @@ import { InputComponent } from './customer-form/input/input.component'
     CustomerFormComponent,
     InputComponent,
   ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule
-  ],
+  imports: [BrowserModule, ReactiveFormsModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
