48b24c2e8e38809e63e0080b598ebf36966cb013
Helena Makendengue <helenamakens1013@gmail.com>
2023-03-02 14:45:10 -0600

    checkout component + routing

---
+++ 04 - Routing & Services/02 - Checkout Component and Routing/lemonadestand/src/app/lemonade/cart-icon/cart-icon.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-icon',
+  templateUrl: './cart-icon.component.html',
+  styleUrls: ['./cart-icon.component.css'],
+})
+export class CartIconComponent implements OnInit {
+  cartCount: number = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
